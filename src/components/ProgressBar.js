48d5be533a4cf2d528d30ff31cfbec007474a50a
yaejin12 <pos03167@naver.com>
2024-07-12 12:00:23 +0900

    password

---
+++ src/components/ProgressBar.js
@@ -0,0 +1,12 @@
+import React, { useEffect } from "react";
+import styles from "./ProgressBar.module.scss";
+
+const ProgressBar = () => {
+  return (
+    <div className={styles.progressBar}>
+      <div className={styles.progress} />
+    </div>
+  );
+};
+
+export default ProgressBar;
